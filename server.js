f0cde1be2ca72cf4bebfacccfa1afa44bebe0df3
jatindhiman5053 <85056158+jatindhiman5053@users.noreply.github.com>
2024-07-14 20:18:47 +0530

    backend

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors());
+
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"reactproject"
+})
+
+app.post('/reactproject', (req, res)=> {
+    const sql = "INSERT INTO signup (`name`,`email`,`password`) VALUES (?)";
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.password
+    ];
+
+    db.query(sql, [values], (err, data) =>{
+        if(err){
+            return res.json("Error");
+        }
+        return res.json("Error");
+    })
+})
+
+
+app.listen(8081, ()=>{
+    console.log("listen");
+})
